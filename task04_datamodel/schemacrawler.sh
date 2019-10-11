e4dda64ab50350272c9550c1a7399e4f9102de42
Diego Munguia <diego.munguia@gmail.com>
2019-10-10 18:56:17 -0600

    Changing schema name on task 04

--- task04_datamodel/schemacrawler.sh
+++ task04_datamodel/schemacrawler.sh
@@ -7,7 +7,7 @@ echo "Generating database schema diagram"
 /opt/schemacrawler/schemacrawler.sh \
 --server=mysql --host=db \
 --user=root --password=root \
---database=EnrollmentSys \
+--database=lms \
 --info-level=standard --command=schema \
 --output-format=png \
 --output-file=/share/task04_diagram.png
@@ -17,7 +17,7 @@ echo "Linting schema"
 /opt/schemacrawler/schemacrawler.sh \
 --server=mysql --host=db \
 --user=root --password=root \
---database=EnrollmentSys \
+--database=lms \
 --info-level=standard --command=lint \
 --output-format=html \
 --output-file=/share/task04_lint.html
